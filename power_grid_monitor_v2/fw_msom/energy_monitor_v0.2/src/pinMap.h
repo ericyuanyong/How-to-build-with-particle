94600108d07eca9b926c8f698e11a36a7c032500
ericyuanyong <eric@particle.io>
2025-07-28 09:25:20 +0800

    update the msom fw

---
+++ power_grid_monitor_v2/fw_msom/energy_monitor_v0.2/src/pinMap.h
@@ -0,0 +1,20 @@
+
+#ifndef _PINMAP_H_
+#define _PINMAP_H_
+
+#include "Particle.h"
+
+
+//pin definitions.
+#define ADC_CS          D8
+#define RELAY_LIVE      RX
+#define RELAY_NEUTRAL   TX
+#define LED             D26
+#define DCDC_MODE       D21
+#define BUTTON          A7
+#define DIO_0           A5
+#define DIO_1           A6
+
+
+
+#endif
